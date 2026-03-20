23098445b8349cd0c9ac24cf9477022843cd3303
OtavioCuriel <otavio.curiel345@gmail.com>
2026-03-20 14:31:46 -0300

    primeira aula do desenvolvento react

---
+++ .eslintrc.cjs
@@ -0,0 +1,38 @@
+const { version } = require("react");
+
+module.exports = {
+  root: true,
+  env: { browser: true, node: true, es2021: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/sytlisct-type-checked",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime"
+  ],
+
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["tsconfig.json", "tsconfig.node.json"],
+    tsconfigRootDir: __dirname,
+  },
+  plugings: ["react-refresh", "@typescript-eslint", "react"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "@typescript-eslint/no-unsafe-call": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+  },
+  setting:{
+    react:{
+      version:"detect",
+    }
+  }
+};
