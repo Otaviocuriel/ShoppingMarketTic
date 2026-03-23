d77be2c0411257ed2a9904aa88c475aa0ba7eb10
OtavioCuriel <otavio.curiel345@gmail.com>
2026-03-23 15:15:52 -0300

    colocando os import do backend

--- src/app/view/home.tsx
+++ src/app/view/home.tsx
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
 import Card from "../components/Card";
+import findAll from "../../services/product.service";
 
 const Home = () => {
+	useEffect	(() => {
+		findAll().then((res) => console.log(res))
+	}, [])
+
     return (
         <Card/>
     )
