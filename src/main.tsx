15a1e1e7c0a475ecc792c97cb73482bcd7e185f6
OtavioCuriel <otavio.curiel345@gmail.com>
2026-03-23 15:21:11 -0300

    importando o queryClient

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClientProvider, QueryClient } from 'react-query'
 import App from './App'
 import './styles/index.css'
 
+const queryClient = new QueryClient;
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
+		<QueryClientProvider client={queryClient}></QueryClientProvider>
 		<App />
 	</React.StrictMode>,
 )
