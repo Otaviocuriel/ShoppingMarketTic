9cc78f94db382a99a369c286dc6223220824f8d3
OtavioCuriel <otavio.curiel345@gmail.com>
2026-03-25 18:58:28 -0300

    tentando estilizar a home

--- src/app/view/home.tsx
+++ src/app/view/home.tsx
@@ -1,37 +1,12 @@
-import {useQuery} from "react-query";
+import { useEffect } from "react";
 import Card from "../components/Card";
-import findAll from "../../services/product.service";
-import type { ProductProps } from "../../interfaces/Product";
+import findAll from "../services/product.service"
 
 const Home = () => {
-	//useEffect(() => {
-		//findAll().then((res) => console.log(res));
-  //	})
-
-	const {
-		data: products,
-		isLoading,
-		error,
-	} = useQuery<ProductProps[], Error>("query-products", async () => {
-		return await findAll();
-	}, {
-		retry: false,
-	});
-
-	if (isLoading) return <p className="mt-28 text-center">Carregando produtos...</p>;
-	if (error) return <p className="mt-28 text-center">Erro ao carregar produtos.</p>;
-
-    return (
-		<main className="mt-24 w-full px-3">
-			<div className="flex flex-wrap justify-center gap-3">
-				{products?.map((product: ProductProps) => {
-					return (
-						<Card key={product.id} product={product} />
-					)
-				})}
-			</div>
-		</main>
-	);
+		useEffect(() => {
+			findAll().then((res)=> console.log(res))
+		})
+	return <Card/>;
 };
 
 export default Home;
