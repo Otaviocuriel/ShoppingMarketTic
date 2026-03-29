3d4300638c9698357b786fee6141b9abc6484dd0
OtavioCuriel <otavio.curiel345@gmail.com>
2026-03-29 19:37:04 -0300

    Terminando de configurar

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,20 +1,20 @@
-import type { ProductProps } from "../../interfaces/Product";
+import type { Product, ProductProps } from "../../interfaces/Product";
 
 type CardProps = {
   product: ProductProps;
 };
 
-const Card = ({ product }: CardProps) => {
+const Card = ({item}: Product) => {
   return (
     <div className="flex flex-col items-center justify-center p-2 text-center">
       <div className="flex flex-col items-center justify-center">
         <img
-          src={`/assets/produtos/${product.image}.jpg`}
+          src={`/assets/produtos/${item.image}.jpg`}
           className="h-auto w-48 object-contain"
-          alt={product.name}
+          alt={item.name}
         />
-        <h2 className="mt-6 text-lg font-semibold capitalize text-[#1f1f1f]">{product.name}</h2>
-        <p className="mt-1 text-lg font-medium text-[#2f2f2f]">R$ {product.price.toFixed(2)}</p>
+        <h2 className="mt-6 text-lg font-semibold capitalize text-[#1f1f1f]">{item.name}</h2>
+        <p className="mt-1 text-lg font-medium text-[#2f2f2f]">R$ {item.price.toFixed(2)}</p>
         <button className="mt-4 rounded bg-blue-600 px-7 py-2 text-sm font-semibold text-white transition hover:bg-blue-700">
           Adicionar no Carrinho
         </button>
