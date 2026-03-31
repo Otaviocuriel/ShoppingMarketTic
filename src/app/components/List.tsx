15efe73186706f16b9dacda4cc9f81899d3658a3
OtavioCuriel <otavio.curiel345@gmail.com>
2026-03-31 10:31:54 -0300

    Colocando um novo arquivo list

---
+++ src/app/components/List.tsx
@@ -0,0 +1,29 @@
+import type { ComponentProps } from "react"
+import { twMerge } from "tailwind-merge";
+import { tv, type VariantProps } from "tailwind-variants"
+
+const ListVariant = tv({
+	variants: {
+		variant: {
+			primary: "flex rounded=md px-1 py-4 text-sm capitalize text-gray-700 houver:bg-gray-200"
+		},
+		defaultVariants: {
+			variant: "primary"
+		}
+	}	
+
+})
+
+type ListProps = ComponentProps<"li"> & VariantProps<typeof ListVariant>;
+
+
+const List = ({children, className, onClick, variant, ...props}: ListProps) => {
+	const ListClass = twMerge(ListVariant({variant}), className)
+
+	return
+	 <li className={ListClass} onClick={onClick} {...props}>
+		{children}
+	</li>;
+}
+
+export default List;
