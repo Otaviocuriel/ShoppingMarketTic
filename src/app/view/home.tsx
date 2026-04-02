24547cf9dad68164658d1e68d74b3f524a04ed34
OtavioCuriel <otavio.curiel345@gmail.com>
2026-04-02 17:02:13 -0300

    Colocando o botao de filtro

--- src/app/view/home.tsx
+++ src/app/view/home.tsx
@@ -3,13 +3,17 @@ import ProductService from "../../services/product.service";
 import type { ProductProps } from "../../interfaces/Product";
 import { useQuery } from "react-query";
 import { useState } from "react";
+import { twMerge } from "tailwind-merge";
+
+const menuListVariants = "absolute z-10 mt-1 w-48 rounded-md bg-white py-1 shadow-lg";
 
 type HomeProps = {
   searchTerm: string;
 };
 
 const Home = ({ searchTerm }: HomeProps) => {
-  const[typeFilter,setTypeFilter] = useState("")
+  const[typeFilter,setTypeFilter] = useState("");
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
 
   const listOptionsFilter = [
     {
@@ -28,6 +32,19 @@ const Home = ({ searchTerm }: HomeProps) => {
     return ProductService.findAll(typeFilter);
   });
 
+  const handleFilter = (value: string) => {
+    setTypeFilter(value);
+  };
+
+  const handleFilterButton = () => {
+    setIsOpenMenu(!isOpenMenu);
+  }
+
+  const menuListClasses = twMerge(menuListVariants,
+   isOpenMenu
+    ? "flex items-center flex-col absolute top-60 bg-white rounded-md p-4 shadow-lg shadow-black w-44":
+     "hidden")
+
   const normalizedSearch = searchTerm.trim().toLowerCase();
   const filteredProducts = products?.filter((product) => {
     return product.name.toLowerCase().includes(normalizedSearch);
@@ -35,15 +52,15 @@ const Home = ({ searchTerm }: HomeProps) => {
 
   return (
 		<div className="mt-24 flex w-full flex-col items-center px-4 pb-10 sm:px-6 lg:px-8">
-      <div>
-        <button className="w-24">
+      <div className="mb-6 flex flex-col items-center">
+        <button className="mb-1 w-24 rounded-sm bg-blue-600 py-1 text-xs font-medium text-white" onClick={() => handleFilterButton()}>
           Filtro
         </button>
-        <ul>
+        <ul className={menuListClasses}>
           {listOptionsFilter.map((item) => {
             return (
-              <li key={item.name} className={item.class} >
-                <button type="button" onClick={() => setTypeFilter(item.value)}>
+              <li key={item.name} className={item.class} onClick={(e) => {e.stopPropagation(); handleFilter(item.value); setIsOpenMenu(false)}} >
+                <button type="button" className="text-xs hover:underline" onClick={() => setTypeFilter(item.value)}>
                   {item.name}
                 </button>
               </li>
