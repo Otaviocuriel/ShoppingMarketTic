e300fbea7e436853d4a91d35431eca676bd36d3e
OtavioCuriel <otavio.curiel345@gmail.com>
2026-04-03 10:06:44 -0300

    Colocando a Carinho centralizado

--- src/app/view/ShoppingCart.tsx
+++ src/app/view/ShoppingCart.tsx
@@ -1,8 +1,9 @@
 const ShoppingCart = () => {
     return(
-        <div>
-            <h1>Carrinho de Compras</h1>
-        </div>
+        <div className="mt-24 flex w-full flex-col items-center px-4 pb-10 sm:px-6 lg:px-8">
+            <div className="mt-32 flex h-4/5 justify-center overflow-x-auto"></div>
+            <h1>Carinho de Compras</h1>
+        </div >
     );
 };
 
