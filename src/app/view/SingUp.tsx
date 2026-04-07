fd5394770a974996e2dae2fe77aee9aef26bed6e
OtavioCuriel <otavio.curiel345@gmail.com>
2026-04-07 10:03:17 -0300

    Tela de SingUp feita

--- src/app/view/SingUp.tsx
+++ src/app/view/SingUp.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 
 const SingUp = () => {
@@ -8,9 +9,61 @@ const SingUp = () => {
 		username: "",
 	});
 
-	return(
+	const navigate = useNavigate();
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		void fetch("http://localhost:3000/users", {
+			method: "POST",
+			headers: {"Content-Type": "application/json"},
+			body: JSON.stringify(formData),
+		}).then((res) => res.json())
+		.then(() =>{
+			navigate("/login");
+		})
+	}
+
+	const hndleChangae = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setFormData({ 
+			...formData,
+			[e.target.name]: e.target.value
+		});
+	};
+
+	return ( 
 		<div>
-			
+			<h1>Sing Up</h1>
+			<form onSubmit={(e) => handleSubmit(e)}>
+				<input
+					type="text"
+					placeholder="Username"
+					value={formData.username}
+					name="username"
+					onChange={(e) => hndleChangae(e)}
+				/>
+				<input
+					type="text"
+					placeholder="Username"
+					value={formData.username}
+					name="username"
+					onChange={(e) => hndleChangae(e)}
+				/>
+				<input
+					type="text"
+					placeholder="Email"
+					value={formData.email}
+					name="email"
+					onChange={(e) => hndleChangae(e)}
+				/>
+				<input
+					type="password"
+					placeholder="Password"
+					value={formData.password}
+					name="password"
+					onChange={(e) => hndleChangae(e)}
+				/>
+				<button type="submit">Sing Up</button>
+			</form>
 		</div>
 	)
 
